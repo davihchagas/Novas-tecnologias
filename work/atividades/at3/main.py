98f3aa648e4f74190d3ff592bf4d4aa53add01ca
Davi Chagas <davih.chagas108@gmail.com>
2026-04-17 23:25:48 -0300

    respostas at3

---
+++ work/atividades/at3/main.py
@@ -0,0 +1,11 @@
+from matrix import transpor_matriz, multiplicar_matriz
+
+A = [[1, 2], [3, 4], [5, 6]]
+print("Transposição:")
+print(transpor_matriz(A))
+
+A = [[1, 2], [3, 4]]
+B = [[5, 6], [7, 8]]
+
+print("\nMultiplicação:")
+print(multiplicar_matriz(A, B))
